4f00474d1f3957b571102010a722494d3fbb080c
BensonCheng8787 <112728945+BensonCheng8787@users.noreply.github.com>
2022-11-07 10:37:53 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,230 +1,143 @@
-/**********************************************************************/
 // ENGR-2350 Template Project
-// Name:
-// RIN:
-// This is the base project for several activities and labs throughout
-// the course.  The outline provided below isn't necessarily *required*
-// by a C program; however, this format is required within ENGR-2350
-// to ease debugging/grading by the staff.
-/**********************************************************************/
-
-// We'll always add this include statement. This basically takes the
-// code contained within the "engr_2350_msp432.h" file and adds it here.
 #include "engr2350_msp432.h"
 
-// Add function prototypes here, as needed.
+void PWMInit();
+void ADCInit();
 void GPIOInit();
-void TimerInit();
-void Encoder_ISR();
-void straight(uint16_t target);
-void angle(uint16_t traget);
-
-// Add global variables here, as needed.
-Timer_A_CaptureModeConfig capconfig_l;
-Timer_A_CaptureModeConfig capconfig_r;
-Timer_A_CompareModeConfig comconfig_l;
-Timer_A_CompareModeConfig comconfig_r;
-Timer_A_ContinuousModeConfig config_con;
-Timer_A_UpModeConfig config_up;
-
-int32_t enc_countsL = 0; //Keep track the timer counts since the capture event, track timer counts between encoder edges
-int32_t enc_countsR = 0;
-int32_t enc_trackL = 0;//keep track of timer since capture
-int32_t enc_trackR = 0;
-int32_t timer_sumL = 0; //store summation of wheel speed
-int32_t timer_sumR = 0;
-uint8_t timer_countL = 0; //track number of measurements in the summation variable
-uint8_t timer_countR = 0;
-uint16_t cur_pwmL = 300; //store the current PWM
-uint16_t cur_pwmR = 300;
-uint32_t avgL;
-uint32_t avgR;
-float setPoint=0;
-
-//variable for straight
-uint8_t stop = 1;//stop car
-uint32_t distL;
-uint32_t distR;
-uint32_t enc_totalL;
-uint32_t enc_totalR;
-float circ = 0.60956;//circumfrence of wheel for one enc
-
-//variable for angle
-float circCar = 468.09691;// circumfrence of car
-float radCar = 149.0;//radius of car
+void PWM_ISR();
+void setSpeed();
 
 
+Timer_A_UpModeConfig TA2cfg; // Using P5.7, TA2.2
+Timer_A_CompareModeConfig TA2_ccr;
 
-int main(void) /* Main Function */
+uint8_t timer_flag = 0;
+int16_t pwm_max = 2300; // Maximum limit on PWM output
+int16_t pwm_min = 0; // Minimum limit on PWM output
+int16_t pwm_set = 1200; // Calculated PWM output (control output)
+//left control
+float kpL = 0.05; // proportional control gain
+float error_sumL = 0; // Integral control error summation
+float kiL = 65536; // integral control gain
+//right control
+float kpR = 0.05; // proportional control gain
+float error_sumR = 0; // Integral control error summation
+float kiR = 65536; // integral control gain
+
+
+uint16_t pot_val; // ADC value from potetiometer
+float desired; // Current "setpoint" voltage, from POT
+uint16_t rc_val; // ADC value from RC circuit
+float actual = 0; // Current output voltage from RC circuit
+
+// Main Function
+int main(void)
 {
-    // Add local variables here, as needed.
-
-    // We always call the "SysInit()" first to set up the microcontroller
-    // for how we are going to use it.
     SysInit();
+
     GPIOInit();
-    TimerInit();
+    PWMInit();
+    ADCInit();
 
-    // Place initialization code (or run-once) code here
-    straight(1830);
-    //angle(180);
+    printf("\r\n\nDes. ADC\tDes. V\tAct. ADC\tAct. V\tPWM Set\r\n");
+
     while(1){
-        printf("%d %d %d\r\n", avgL,avgR, stop);
+        // If the PWM has cycled, request an ADC sample
+        if(timer_flag){
+            // Add ADC conversion code here
+            ADC14_toggleConversionTrigger();
+            while(ADC14_isBusy()){}
+            //part 1
+            pot_val = ADC14_getResult(ADC_MEM14);
+            desired = 1.0*pot_val*3.3/16384;
+            //part2
+            rc_val = ADC14_getResult(ADC_MEM15);
+            actual = 1.0*rc_val*3.3/16384;
 
 
-    }
-}
+            // *********** CONTROL ROUTINE *********** //
+            error_sum += desired-actual; // perform "integration"
+            pwm_set = kp*(pwm_max-pwm_min)/desired-ki*error_sum; // PI control equation
+            if(pwm_set > pwm_max) pwm_set = pwm_max;  // Set limits on the pwm control output
+            if(pwm_set < pwm_min) pwm_set = pwm_min;
+            Timer_A_setCompareValue(TIMER_A2_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_2,pwm_set); // enforce pwm control output
+            // ********* END CONTROL ROUTINE ********* //
 
-// Add function declarations here as needed
-void GPIOInit() {
 
 
-    GPIO_setAsOutputPin(3,GPIO_PIN6); //right enable
-    GPIO_setAsOutputPin(5,GPIO_PIN5); //right direction
-    GPIO_setAsOutputPin(3,GPIO_PIN7); //left enable
-    GPIO_setAsOutputPin(5,GPIO_PIN4); //left direction
+            printf("\r%5u\t   %1.3f\t  %5u\t   %1.3f\t%5u",pot_val,desired,rc_val,actual,pwm_set); // report
+            __delay_cycles(240e3); // crude delay to prevent this from running too quickly
+            timer_flag = 0; // Mark that we've performed the control loop
 
-    GPIO_setAsPeripheralModuleFunctionOutputPin(2 , GPIO_PIN6 , GPIO_PRIMARY_MODULE_FUNCTION ); //pwm speed right, timer 0 register 3
-    GPIO_setAsPeripheralModuleFunctionOutputPin( 2 , GPIO_PIN7 , GPIO_PRIMARY_MODULE_FUNCTION ); //pwm speed left, timer 0 register 4
-    GPIO_setAsPeripheralModuleFunctionInputPin(10 , GPIO_PIN4 , GPIO_PRIMARY_MODULE_FUNCTION ); //timer 3 register 0
-    GPIO_setAsPeripheralModuleFunctionInputPin(10 , GPIO_PIN5 , GPIO_PRIMARY_MODULE_FUNCTION ); //timer 3 register 1
-
-    GPIO_setOutputHighOnPin(3, GPIO_PIN6 | GPIO_PIN7);
-    GPIO_setOutputLowOnPin(5, GPIO_PIN4 | GPIO_PIN5);
-}
-
-void TimerInit() {
-    //timer up mode
-    config_up.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
-    config_up.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;
-    config_up.timerPeriod = 800;
-    Timer_A_configureUpMode(TIMER_A0_BASE, &config_up);
-
-    //timer continuous mode
-    config_con.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
-    config_con.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;
-    config_con.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_ENABLE;
-    Timer_A_configureContinuousMode(TIMER_A3_BASE, &config_con);
-    Timer_A_registerInterrupt(TIMER_A3_BASE ,TIMER_A_CCRX_AND_OVERFLOW_INTERRUPT ,Encoder_ISR);
-
-    //capture mode left
-    capconfig_l.captureRegister = TIMER_A_CAPTURECOMPARE_REGISTER_0;
-    capconfig_l.captureMode = TIMER_A_CAPTUREMODE_RISING_EDGE;
-    capconfig_l.captureInputSelect = TIMER_A_CAPTURE_INPUTSELECT_CCIxA;
-    capconfig_l.synchronizeCaptureSource = TIMER_A_CAPTURE_SYNCHRONOUS;
-    capconfig_l.captureInterruptEnable = TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE;
-    Timer_A_registerInterrupt(TIMER_A3_BASE ,TIMER_A_CCR0_INTERRUPT ,Encoder_ISR);
-    Timer_A_initCapture(TIMER_A3_BASE, &capconfig_l);
-
-    //capture mode right
-    capconfig_r.captureRegister = TIMER_A_CAPTURECOMPARE_REGISTER_1;
-    capconfig_r.captureMode = TIMER_A_CAPTUREMODE_RISING_EDGE;
-    capconfig_r.captureInputSelect = TIMER_A_CAPTURE_INPUTSELECT_CCIxA;
-    capconfig_r.synchronizeCaptureSource = TIMER_A_CAPTURE_SYNCHRONOUS;
-    capconfig_r.captureInterruptEnable = TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE;
-    //Timer_A_registerInterrupt(TIMER_A3_BASE ,TIMER_A_CCR0_INTERRUPT ,Encoder_ISR);
-    Timer_A_initCapture(TIMER_A3_BASE, &capconfig_r);
-
-    //compare mode left
-    comconfig_l.compareOutputMode = TIMER_A_OUTPUTMODE_RESET_SET;
-    comconfig_l.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_4;
-    comconfig_l.compareValue = cur_pwmL;
-    Timer_A_initCompare(TIMER_A0_BASE, &comconfig_l);
-
-    //compare mode right
-    comconfig_r.compareOutputMode = TIMER_A_OUTPUTMODE_RESET_SET;
-    comconfig_r.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_3;
-    comconfig_r.compareValue = cur_pwmR;
-    Timer_A_initCompare(TIMER_A0_BASE, &comconfig_r);
-
-    Timer_A_startCounter(TIMER_A3_BASE, TIMER_A_CONTINUOUS_MODE);
-    Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
-}
-
-void Encoder_ISR() {
-    if(Timer_A_getEnabledInterruptStatus(TIMER_A3_BASE)){
-        Timer_A_clearInterruptFlag(TIMER_A3_BASE);
-        enc_trackL+=65536;
-        enc_trackR+=65536;
-    }
-    //left capture
-    else if (Timer_A_getCaptureCompareEnabledInterruptStatus(
-            TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_0)
-                    & TIMER_A_CAPTURECOMPARE_INTERRUPT_FLAG) {
-        Timer_A_clearCaptureCompareInterrupt(TIMER_A3_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
-        //calculate timer between edges
-        enc_countsL = enc_trackL + Timer_A_getCaptureCompareCount(TIMER_A3_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
-        enc_trackL = -Timer_A_getCaptureCompareCount(TIMER_A3_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
-        timer_sumL+= enc_countsL;
-        timer_countL++;
-        enc_totalL++;
-        if(timer_countL==6){
-            avgL = timer_sumL/6;
-            if(!stop&&(timer_sumL/6)>setPoint){
-                cur_pwmL++;
-            }
-            else if(!stop&&(timer_sumL)/6<setPoint){
-                cur_pwmL--;
-            }
-            Timer_A_setCompareValue(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_3,cur_pwmL);
-            timer_sumL=0;
-            timer_countL=0;
-        }
-    }
-    //right capture
-    else if (Timer_A_getCaptureCompareEnabledInterruptStatus(
-            TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_1)
-                    & TIMER_A_CAPTURECOMPARE_INTERRUPT_FLAG) {
-        Timer_A_clearCaptureCompareInterrupt(TIMER_A3_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_1);
-        //calculate timer between edges
-        enc_countsR = enc_trackR + Timer_A_getCaptureCompareCount(TIMER_A3_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_1);
-        enc_trackR = -Timer_A_getCaptureCompareCount(TIMER_A3_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_1);
-        timer_sumR+= enc_countsR;
-        timer_countR++;
-        enc_totalR++;
-        if(timer_countR==6){
-            avgR = timer_sumR/6;
-            if(!stop&&(timer_sumR/6)>setPoint){
-                cur_pwmR++;
-            }
-            else if(!stop&&(timer_sumR)/6<setPoint){
-                cur_pwmR--;
-            }
-            Timer_A_setCompareValue(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_4, cur_pwmR);
-            timer_sumR=0;
-            timer_countR=0;
         }
     }
 }
 
-void straight(uint16_t target){
-    setPoint = 60000;
-    stop =0;
-    enc_totalR =0;
-    //enc_totalL = 0;
-    uint16_t distance =0;
-    while(distance< target){
-        distance = circ*enc_totalR;
-        printf("%d\r\n",distance);
-    }
-    stop = 1;
-    setPoint = 97500;
+void setSpeed(){
+    //get ADC sample
+    ADC14_toggleConversionTrigger();
+    while(ADC14_isBusy()){}
+    //left pot
+    pot_valL = ADC14_getResult(ADC_MEM14);
+    desiredL = 1.0*pot_val*3.3/16384;
+    //right pot
+    pot_valR = ADC14_getResult(ADC_MEM15);
+    desiredR = 1.0*pot_val*3.3/16384;
+
+    //conversion, set PWM duty cycle to match voltage
+    error_sum += desired-actual; // perform "integration"
+    pwm_set = kp*(pwm_max-pwm_min)/desired-ki*error_sum; // PI control equation
+    if(pwm_set > pwm_max) pwm_set = pwm_max;  // Set limits on the pwm control output
+    if(pwm_set < pwm_min) pwm_set = pwm_min;
+    Timer_A_setCompareValue(TIMER_A2_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_2,pwm_set); // enforce pwm control output
+
+
 }
 
-void angle(uint16_t target){
-    uint16_t dist = 0;
-    uint16_t targetDist;
-    GPIO_setOutputLowOnPin(5,GPIO_PIN5);//right forward
-    GPIO_setOutputHighOnPin(5,GPIO_PIN4);//left backward
-    targetDist = circCar*360/target;
-    setPoint = 45000;
-    stop = 0;
-    while(dist<targetDist){
-        dist = circ*enc_totalR;
-        printf("turning\r\n");
-    }
-    setPoint = 97500;
-    stop = 1;
+
+
+
+void GPIOInit(){
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN6,GPIO_PRIMARY_MODULE_FUNCTION); // PWM output right
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN7,GPIO_PRIMARY_MODULE_FUNCTION); // left
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P6, GPIO_PIN1,GPIO_TERTIARY_MODULE_FUNCTION); //read in pot v
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P6, GPIO_PIN0,GPIO_TERTIARY_MODULE_FUNCTION);
 }
 
-// Add interrupt functions last so they are easy to find
+
+void ADCInit(){
+    // Activity Stuff...
+    ADC14_enableModule();
+    ADC14_initModule(ADC_CLOCKSOURCE_SMCLK, ADC_PREDIVIDER_4, ADC_DIVIDER_1, 0);
+    ADC14_configureConversionMemory(ADC_MEM14, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A14, 0);//p6 pin1
+    ADC14_configureConversionMemory(ADC_MEM15, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A15, 0);//p6 pin0
+    //ADC14_configureSingleSampleMode(ADC_MEM14, true); // part 1
+    ADC14_configureMultiSequenceMode(ADC_MEM14, ADC_MEM15, true);//part2
+    //ADC14_setSampleHoldTrigger(ADC_TRIGGER_SOURCE6, false);
+    ADC14_enableSampleTimer(ADC_MANUAL_ITERATION);
+    ADC14_enableConversion();
+}
+
+void PWMInit(){
+    // Set up Timer_A2 to run at 10 kHz
+    TA2cfg.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
+    TA2cfg.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_10;
+    TA2cfg.timerPeriod = 2400;
+    TA2cfg.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_ENABLE;
+    Timer_A_configureUpMode(TIMER_A2_BASE,&TA2cfg);
+
+    // Configure TA2.CCR2 for PWM generation
+    TA2_ccr.compareOutputMode = TIMER_A_OUTPUTMODE_SET_RESET;
+    TA2_ccr.compareValue = pwm_set;
+    TA2_ccr.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_2;
+    Timer_A_initCompare(TIMER_A2_BASE,&TA2_ccr);
+
+    Timer_A_registerInterrupt(TIMER_A2_BASE,TIMER_A_CCRX_AND_OVERFLOW_INTERRUPT,PWM_ISR);
+    Timer_A_startCounter(TIMER_A2_BASE,TIMER_A_UP_MODE);
+}
+
+void PWM_ISR(){
+    Timer_A_clearInterruptFlag(TIMER_A2_BASE);
+    timer_flag = 1;
+}
+
