833eadccb8ce38a01d098a116cc7f877e8ece5f3
BensonCheng8787 <112728945+BensonCheng8787@users.noreply.github.com>
2022-11-20 18:10:12 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
-/**********************************************************************/
-// ENGR-2350 Template Project
-// Name: XXXX
-// RIN: XXXX
-// This is the base project for several activities and labs throughout
-// the course.  The outline provided below isn't necessarily *required*
-// by a C program; however, this format is required within ENGR-2350
-// to ease debugging/grading by the staff.
-/**********************************************************************/
-
-// We'll always add this include statement. This basically takes the
-// code contained within the "engr_2350_msp432.h" file and adds it here.
 #include "engr2350_msp432.h"
 
 void GPIOInit();
+void TimerInit();
+void ADCInit();
+void Encoder_ISR();
+void T1_100ms_ISR();
 void I2CInit();
+void Distance();
 uint16_t readCompass();
 uint16_t readRanger();
 void I2C_writeData( uint32_t moduleInstance , uint8_t PeriphAddress , uint8_t StartReg , uint8_t * data , uint8_t len );
 void I2C_readData( uint32_t moduleInstance , uint8_t PeriphAddress , uint8_t StartReg , uint8_t * data , uint8_t len );
+
+
+Timer_A_UpModeConfig TA0cfg;
+Timer_A_UpModeConfig TA1cfg;
+Timer_A_ContinuousModeConfig TA3cfg;
+Timer_A_CompareModeConfig TA0_ccr3;
+Timer_A_CompareModeConfig TA0_ccr4;
+Timer_A_CaptureModeConfig TA3_ccr0;
+Timer_A_CaptureModeConfig TA3_ccr1;
+
+
 
 uint8_t datacom[2];
 uint8_t lencom=2;
@@ -27,34 +31,103 @@ uint8_t  dataran[2];
 uint8_t lenran=2;
 uint16_t range;
 
-
-
 eUSCI_I2C_MasterConfig config;
 
 
-// Add global variables here, as needed.
+
+// Encoder total events
+uint32_t enc_total_L,enc_total_R;
+// Speed measurement variables
+float potSpeed,desiredSpeed,desiredSpeedL,desiredSpeedR,actualSpeedL,actualSpeedR;
+float potDist,desiredDist,desiredDistRadius;
+int32_t Tach_L_count,Tach_L,Tach_L_sum,Tach_L_sum_count,Tach_L_avg; // Left wheel
+int32_t Tach_R_count,Tach_R,Tach_R_sum,Tach_R_sum_count,Tach_R_avg; // Right wheel
+
+uint32_t errorSumL, errorSumR, state, dw, speedL, speedR, pwm_setL, pwm_setR, dv,
+ki;
+int32_t pwm_max = 720;
+int32_t pwm_min = 70;
+
+uint16_t targetDist = 50;
+float kdp = 1;
 
 
-int main(void) /* Main Function */
+uint8_t run_control = 0; // Flag to denote that 100ms has passed and control should be run.
+
+int main(void)
 {
-    // Add local variables here, as needed.
-
-    // We always call the "SysInit()" first to set up the microcontroller
-    // for how we are going to use it.
     SysInit();
     GPIOInit();
+    ADCInit();
+    TimerInit();
     I2CInit();
-    // Place initialization code (or run-once) code here
+
+    __delay_cycles(24e6);
+    GPIO_setOutputHighOnPin(GPIO_PORT_P3,GPIO_PIN6|GPIO_PIN7);//motor back on
 
     while(1){
-        // Place code that runs continuously in here
+        if(run_control){    // If 100 ms has passed
+            run_control = 0;    // Reset the 100 ms flag
+            //printf("running\r\n");
+            Distance();
+            __delay_cycles(240e3);
 
-        printf("b\r\n");
-        printf("Compass: %4u\tRanger: %4u\r\n",readCompass(),readRanger());
-        printf("a\r\n");
-
-         __delay_cycles(2.4e6); // Wait 1/10 of a second
+        }
     }
+}
+
+void Distance(){
+    printf("running1\r\n");
+    uint16_t curDist = readRanger();
+    printf("running2\r\n");
+    int8_t dir = 0; // 1 = forward and -1 = backward
+    int16_t diffDist = targetDist - curDist;
+    if(diffDist>0){//too close
+        kdp = kdp*-1;
+        dir = -1;
+    }
+    else if(diffDist<0){//too far
+        diffDist = diffDist*-1;
+        dir = 1;
+    }
+    pwm_setL = (kdp*diffDist)/80 * 800;
+    pwm_setR = (kdp*diffDist)/80 * 800;
+    /*if(dir>0){//forward
+        GPIO_setOutputLowOnPin(GPIO_PORT_P5,GPIO_PIN4|GPIO_PIN5);
+    }
+    else{//backward
+        GPIO_setOutputHighOnPin(GPIO_PORT_P5,GPIO_PIN4|GPIO_PIN5);
+    }*/
+    Timer_A_setCompareValue(TIMER_A0_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_4,pwm_setL); // enforce pwm control output
+    Timer_A_setCompareValue(TIMER_A0_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_3,pwm_setR); // enforce pwm control output
+    printf("pwm_setL: %d, pwm_setR: %d ,cur: %d  diff: %d dir: %d \r\n", pwm_setL, pwm_setR, curDist, diffDist, dir);
+}
+
+
+void ADCInit(){
+    // Add your ADC initialization code here.
+    ADC14_enableModule();
+    ADC14_initModule(ADC_CLOCKSOURCE_SMCLK, ADC_PREDIVIDER_1, ADC_DIVIDER_4, 0);
+    ADC14_configureConversionMemory(ADC_MEM0, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A12, false);//p4 pin1(speed)
+    ADC14_configureConversionMemory(ADC_MEM1, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A9, false);//p4 pin4(turn)
+    ADC14_enableSampleTimer(ADC_MANUAL_ITERATION);
+    ADC14_configureMultiSequenceMode(ADC_MEM0, ADC_MEM1, false);//part2
+    ADC14_enableConversion();
+    //  Don't forget the GPIO, either here or in GPIOInit()!!
+
+
+}
+
+void GPIOInit(){
+    GPIO_setAsOutputPin(GPIO_PORT_P5,GPIO_PIN4|GPIO_PIN5);   // Motor direction pins
+    GPIO_setAsOutputPin(GPIO_PORT_P3,GPIO_PIN6|GPIO_PIN7);   // Motor enable pins
+
+    //compass and ranger
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P6,GPIO_PIN6,GPIO_SECONDARY_MODULE_FUNCTION);
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P6,GPIO_PIN7,GPIO_SECONDARY_MODULE_FUNCTION);
+
+    GPIO_setOutputLowOnPin(GPIO_PORT_P5,GPIO_PIN4|GPIO_PIN5);   // Motors set to forward
+    GPIO_setOutputLowOnPin(GPIO_PORT_P3,GPIO_PIN6|GPIO_PIN7);   // Motors are OFF
 }
 
 void I2CInit(){
@@ -66,10 +139,102 @@ void I2CInit(){
     I2C_enableModule(EUSCI_B3_BASE);
 }
 
-void GPIOInit(){
-    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P6,GPIO_PIN6,GPIO_SECONDARY_MODULE_FUNCTION);
-    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P6,GPIO_PIN7,GPIO_SECONDARY_MODULE_FUNCTION);
+void TimerInit(){
+    // Configure PWM timer for 30 kHz
+    TA0cfg.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
+    TA0cfg.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;
+    TA0cfg.timerPeriod = 800;
+    Timer_A_configureUpMode(TIMER_A0_BASE,&TA0cfg);
+    // Configure TA0.CCR3 for PWM output
+    TA0_ccr3.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_3;
+    TA0_ccr3.compareOutputMode = TIMER_A_OUTPUTMODE_RESET_SET;
+    TA0_ccr3.compareValue = 0;
+    Timer_A_initCompare(TIMER_A0_BASE,&TA0_ccr3);
+    // Configure TA0.CCR4 for PWM output
+    TA0_ccr4.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_4;
+    TA0_ccr4.compareOutputMode = TIMER_A_OUTPUTMODE_RESET_SET;
+    TA0_ccr4.compareValue = 0;
+    Timer_A_initCompare(TIMER_A0_BASE,&TA0_ccr4);
+    // Configure Encoder timer in continuous mode
+    TA3cfg.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
+    TA3cfg.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;
+    TA3cfg.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_ENABLE;
+    Timer_A_configureContinuousMode(TIMER_A3_BASE,&TA3cfg);
+    // Configure TA3.CCR0 for Encoder measurement
+    TA3_ccr0.captureRegister = TIMER_A_CAPTURECOMPARE_REGISTER_0;
+    TA3_ccr0.captureMode = TIMER_A_CAPTUREMODE_RISING_EDGE;
+    TA3_ccr0.captureInputSelect = TIMER_A_CAPTURE_INPUTSELECT_CCIxA;
+    TA3_ccr0.synchronizeCaptureSource = TIMER_A_CAPTURE_SYNCHRONOUS;
+    TA3_ccr0.captureInterruptEnable = TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE;
+    Timer_A_initCapture(TIMER_A3_BASE,&TA3_ccr0);
+    // Configure TA3.CCR1 for Encoder measurement
+    TA3_ccr1.captureRegister = TIMER_A_CAPTURECOMPARE_REGISTER_1;
+    TA3_ccr1.captureMode = TIMER_A_CAPTUREMODE_RISING_EDGE;
+    TA3_ccr1.captureInputSelect = TIMER_A_CAPTURE_INPUTSELECT_CCIxA;
+    TA3_ccr1.synchronizeCaptureSource = TIMER_A_CAPTURE_SYNCHRONOUS;
+    TA3_ccr1.captureInterruptEnable = TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE;
+    Timer_A_initCapture(TIMER_A3_BASE,&TA3_ccr1);
+    // Register the Encoder interrupt
+    Timer_A_registerInterrupt(TIMER_A3_BASE,TIMER_A_CCR0_INTERRUPT,Encoder_ISR);
+    Timer_A_registerInterrupt(TIMER_A3_BASE,TIMER_A_CCRX_AND_OVERFLOW_INTERRUPT,Encoder_ISR);
+    // Configure 10 Hz timer
+    TA1cfg.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
+    TA1cfg.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_64;
+    TA1cfg.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_ENABLE;
+    TA1cfg.timerPeriod = 37500;
+    Timer_A_configureUpMode(TIMER_A1_BASE,&TA1cfg);
+    Timer_A_registerInterrupt(TIMER_A1_BASE,TIMER_A_CCRX_AND_OVERFLOW_INTERRUPT,T1_100ms_ISR);
+    // Start all the timers
+    Timer_A_startCounter(TIMER_A0_BASE,TIMER_A_UP_MODE);
+    Timer_A_startCounter(TIMER_A1_BASE,TIMER_A_UP_MODE);
+    Timer_A_startCounter(TIMER_A3_BASE,TIMER_A_CONTINUOUS_MODE);
 }
+
+
+void Encoder_ISR(){
+    // If encoder timer has overflowed...
+    if(Timer_A_getEnabledInterruptStatus(TIMER_A3_BASE) == TIMER_A_INTERRUPT_PENDING){
+        Timer_A_clearInterruptFlag(TIMER_A3_BASE);
+        Tach_R_count += 65536;
+        Tach_L_count += 65536;
+    // Otherwise if the Left Encoder triggered...
+    }else if(Timer_A_getCaptureCompareEnabledInterruptStatus(TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_0)&TIMER_A_CAPTURECOMPARE_INTERRUPT_FLAG){
+        Timer_A_clearCaptureCompareInterrupt(TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_0);
+        enc_total_R++;   // Increment the total number of encoder events for the left encoder
+        // Calculate and track the encoder count values
+        Tach_R = Tach_R_count + Timer_A_getCaptureCompareCount(TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_0);
+        Tach_R_count = -Timer_A_getCaptureCompareCount(TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_0);
+        // Sum values for averaging
+        Tach_R_sum_count++;
+        Tach_R_sum += Tach_R;
+        // If 6 values have been received, average them.
+        if(Tach_R_sum_count == 6){
+            Tach_R_avg = Tach_R_sum/6;
+            Tach_R_sum_count = 0;
+            Tach_R_sum = 0;
+        }
+    // Otherwise if the Right Encoder triggered...
+    }else if(Timer_A_getCaptureCompareEnabledInterruptStatus(TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_1)&TIMER_A_CAPTURECOMPARE_INTERRUPT_FLAG){
+        Timer_A_clearCaptureCompareInterrupt(TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_1);
+        enc_total_L++;
+        Tach_L = Tach_L_count + Timer_A_getCaptureCompareCount(TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_1);
+        Tach_L_count = -Timer_A_getCaptureCompareCount(TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_1);
+        Tach_L_sum_count++;
+        Tach_L_sum += Tach_L;
+        if(Tach_L_sum_count == 6){
+            Tach_L_avg = Tach_L_sum/6;
+            Tach_L_sum_count = 0;
+            Tach_L_sum = 0;
+        }
+    }
+}
+
+void T1_100ms_ISR(){
+    Timer_A_clearInterruptFlag(TIMER_A1_BASE);
+    run_control = 1;
+
+}
+
 
 uint16_t readCompass(){
     if(lencom >= 2) {
@@ -84,10 +249,11 @@ uint16_t readCompass(){
 
 uint16_t readRanger(){
     if(lenran >= 2) {
+        printf("read1\r\n");
         I2C_readData( EUSCI_B3_BASE , 0x70 , 2 , dataran , lenran );
         range = (dataran[0]<<8);
         range |= dataran[1];
-
+        printf("read2\r\n");
         dataran[0]=0x51;
         I2C_writeData(EUSCI_B3_BASE,0x70,0,dataran,1);
         return range;
@@ -95,8 +261,6 @@ uint16_t readRanger(){
     else
         return 0;
 }
-
-
 
 void I2C_writeData(uint32_t moduleInstance
                   ,uint8_t PeriphAddress
